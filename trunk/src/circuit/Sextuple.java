2bbe2b160cc27fc491c1f8eb80018a72e8eccd85
amarnirm@gmail.com <amarnirm@gmail.com@136a246c-9910-8519-c4b4-5b68abe50256>
2011-07-27 10:52:26 +0000

    added comments to the Project.

--- trunk/src/circuit/Sextuple.java
+++ trunk/src/circuit/Sextuple.java
@@ -11,29 +11,30 @@ public class Sextuple {
 	public ArrayList<ArrayList<SecretShare>> secretShares;
 	public int index;
 	public int next;
-	public int type; //0 = addition, 1 = multiplication, 2 = constmult, 3 = maximum
+	public int type; // 0 = addition, 1 = multiplication, 2 = constmult, 3 =
+						// maximum
 	public ArrayList<SecretShare> output;
 	public int constant;
 
-public Sextuple(ArrayList<ArrayList<SecretShare>> secretShares,int index,int next,int type) {
-	this.secretShares = secretShares;
-	this.index = index;
-	this.next = next;
-	this.type = type;
-}
+	public Sextuple(ArrayList<ArrayList<SecretShare>> secretShares, int index,
+			int next, int type) {
+		this.secretShares = secretShares;
+		this.index = index;
+		this.next = next;
+		this.type = type;
+	}
 
-public Sextuple(ArrayList<ArrayList<SecretShare>> secretShares,int index,int next,int type,int constant) {
-	this.secretShares = secretShares;
-	this.index = index;
-	this.next = next;
-	this.type = type;
-	this.constant = constant;
-}
+	public Sextuple(ArrayList<ArrayList<SecretShare>> secretShares, int index,
+			int next, int type, int constant) {
+		this.secretShares = secretShares;
+		this.index = index;
+		this.next = next;
+		this.type = type;
+		this.constant = constant;
+	}
 
-public void changeNext(int next) {
-	this.next = next;
-}
-
-
+	public void changeNext(int next) {
+		this.next = next;
+	}
 
 }
--- trunk/src/party/Party.java
+++ trunk/src/party/Party.java
@@ -11,6 +11,7 @@ public class Party {
 
 	public final static int t = 1; // number of Affiliates rivals
 	public final static int n = 3; // number of parties
+	//public final static int n >= 2*t + 1; // number of parties
 	// NOTICE - the parties are indexed by 1, .. , n (not by zero)
 	public static int field = 0;
 
